0f6676983aff66173404a3d81084345c97765792
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2025-01-29 13:39:59 +0300

    JoinedAt field auto generate

---
+++ TextShare.API/Migrations/20250129103855_GroupMemberJoinedAdFiledAutoTime.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TextShare.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class GroupMemberJoinedAdFiledAutoTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
