eff2fb9ed5ba0b15a0397f3bfc48e112470dfb54
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2025-01-29 13:34:07 +0300

    Added category repositories.

---
+++ TextShare.DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TextShare.DAL.Data;
+using TextShare.DAL.Interfaces;
+using TextShare.Domain.Entities.TextFiles;
+
+namespace TextShare.DAL.Repositories
+{
+    /// <summary>
+    /// Репозиторий категорий.
+    /// </summary>
+    public class CategoryRepository : IRepository<Category>
+    {
+        private readonly DbSet<Category> _categories;
+
+        public CategoryRepository(TextShareContext context)
+        {
+            _categories = context.Categories;
+        }
+
+        public async Task<List<Category>> GetAllAsync(params string[] includes)
+        {
+            IQueryable<Category> query = _categories.AsQueryable();
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<Category?> GetAsync(int id, params string[] includes)
+        {
+            IQueryable<Category> query = _categories.AsQueryable();
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return await query.FirstOrDefaultAsync(c => c.CategoryId == id);
+        }
+
+        public async Task<List<Category>> FindAsync(Expression<Func<Category, bool>> predicate)
+        {
+            return await _categories.Where(predicate).ToListAsync();
+        }
+
+        public async Task<Category> CreateAsync(Category entity)
+        {
+            var res = await _categories.AddAsync(entity);
+            return res.Entity;
+        }
+
+        public async Task<Category> UpdateAsync(Category entity)
+        {
+            var res = _categories.Update(entity);
+            await Task.CompletedTask;
+            return res.Entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var category = await _categories.FindAsync(id);
+            if (category == null) return false;
+            _categories.Remove(category);
+            return true;
+        }
+
+        public async Task<bool> ContainsAsync(Category entity)
+        {
+            return await _categories.AnyAsync(c => c.Name == entity.Name);
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _categories.CountAsync();
+        }
+    }
+}
