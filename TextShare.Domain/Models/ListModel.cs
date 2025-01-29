1fcbeed3c830310a27a6315c57026663f27e5ca9
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2025-01-30 00:58:15 +0300

    Added models for data packaging

---
+++ TextShare.Domain/Models/ListModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextShare.Domain.Models
+{
+    /// <summary>
+    /// Класс для коллекции ответов.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ListModel<T>
+    {
+        public List<T> Items { get; set; } = new();
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; } = 1;
+
+        public ListModel () { }
+        public ListModel(List<T> items, int currentPage, int totalPages)
+        {
+            Items = items;
+            CurrentPage = currentPage;
+            TotalPages = totalPages;
+        }
+
+        public override string ToString()
+        {
+            string str = $"ListModel: Count items - {Items.Count}. " +
+                $"Current Page - {CurrentPage}. Total Pages - {TotalPages}.";
+            return str;
+        }
+    }
+}
---
+++ TextShare.Domain/Models/ResponseData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextShare.Domain.Models
+{
+    /// <summary>
+    /// Класс для размещения данных.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ResponseData<T>
+    {
+        public T? Data { get; set; } = default;
+        public bool Success { get; set; } = true;
+        public string? ErrorMessage { get; set; } = null;
+
+        public ResponseData() { }
+
+        public ResponseData(T data, bool success  = true, string? errorMessage = null) :this()
+        {
+            Data = data;
+            Success = success;
+            ErrorMessage = errorMessage;
+        }
+
+        public override string ToString()
+        {
+            string info = $"Response data type: {(Data == null ? "Null" : Data.GetType().ToString())}\n";
+            info += $"Success: {Success}\n";
+            if (ErrorMessage != null) info += $"Error message: {ErrorMessage}";
+            return info ;
+        }
+    }
+}
