8e1d399730aba554e06c0dc45dd2b1e00942b83f
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2025-02-03 20:33:37 +0300

    Add UI. Start

---
+++ TextShare.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TextShare.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
