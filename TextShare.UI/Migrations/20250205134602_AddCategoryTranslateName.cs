23ff3b4a8373c0fb567e525b18f66d2070666c50
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2025-02-05 17:17:46 +0300

    The top bar and footer have been moved to partial views

---
+++ TextShare.UI/Migrations/20250205134602_AddCategoryTranslateName.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TextShare.UI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryTranslateName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TranslateName",
+                table: "Categories",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_TranslateName",
+                table: "Categories",
+                column: "TranslateName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_TranslateName",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "TranslateName",
+                table: "Categories");
+        }
+    }
+}
