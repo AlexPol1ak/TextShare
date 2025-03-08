6d780fb82825df0ccb718f819affe1acc59b6f0e
Король Александр <zarjrw1994@gmail.com>
2025-03-08 20:34:56 +0300

    A detailed group display has been implemented. Updating a group. Deleting a group.

---
+++ TextShare.Domain/Models/EntityModels/GroupModels/GroupUpdateModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextShare.Domain.Entities.Groups;
+
+namespace TextShare.Domain.Models.EntityModels.GroupModels
+{
+    /// <summary>
+    /// Модель обновления группы.
+    /// </summary>
+    public class GroupUpdateModel
+    {
+        public int GroupId { get; set; }
+        [Required(ErrorMessage = "Поле обязательно для заполнения.")]
+        [StringLength(100, ErrorMessage = "Имя должно содержать не более 100 символов.")]
+        [RegularExpression(@"^[a-zA-Zа-яА-Я0-9_#\-\s]+$",
+            ErrorMessage = "Имя группы может содержать буквы (латинские или русские), цифры, пробелы, символы _ и #.")]
+        public string Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Описание должно содержать не более 500 символов.")]
+
+        public string? ImageUri { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
